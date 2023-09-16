f466a78b1591f5bde25981a04be58a7815fff3d7
Nilesh Mishra <91262795+redsasori@users.noreply.github.com>
2023-09-16 08:57:41 +0530

    Update desktopintel.sh

--- desktopintel.sh
+++ desktopintel.sh
@@ -3,8 +3,6 @@ echo 'Welcome to Fedora Desktop Setup Script made by NSM'
 
 echo 'Now we will install all the necessary dnf things for setting up Fedora'
 
-#axel_git
-sudo dnf install -y axel
 sudo dnf install -y python3
 sudo dnf install -y python3-pip
 
