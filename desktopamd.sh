ecfbfc3ea7295a247a86a3d6debcea0c114ab7db
Nilesh Mishra <91262795+redsasori@users.noreply.github.com>
2023-09-27 22:26:29 +0530

    Update desktopamd.sh

--- desktopamd.sh
+++ desktopamd.sh
@@ -1,18 +1,7 @@
-echo 'Welcome to Fedora Desktop Setup Script made by NSM'
-
+scriptby=$(figlet "Script by NSM")
+echo "$scriptby"
 
 echo 'Now we will install all the necessary dnf things for setting up Fedora'
-
-sudo dnf install -y python3-pip;
-pip3 install pyfiglet;
-
-scriptby=$(python3 - <<EOF
-import pyfiglet
-result = pyfiglet.figlet_format("Script by NSM")
-print(result)
-EOF
-
-echo "$scriptby"
 
 #dnfconfig
 echo 'Making some necessary changes in dnf config'
