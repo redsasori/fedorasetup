3eaf220e20bb4b942eb2902b18ad629bb8eed584
Nilesh Mishra <91262795+redsasori@users.noreply.github.com>
2023-09-27 19:36:01 +0530

    Update desktopintel.sh

--- desktopintel.sh
+++ desktopintel.sh
@@ -3,11 +3,11 @@ echo 'Welcome to Fedora Desktop Setup Script made by NSM'
 
 echo 'Now we will install all the necessary dnf things for setting up Fedora'
 
-sudo dnf install -y python3
-sudo dnf install -y python3-pip
+sudo dnf install -y python3-pip;
+pip3 install pyfiglet
+
 
 #dnfconfig
-
 echo 'Making some necessary changes in dnf config'
 sudo cp -a dnf.conf /etc/dnf/dnf.conf
 
@@ -18,21 +18,15 @@ sudo cp -a config /etc/selinux/config
 sudo dnf install -y fedora-workstation-repositories
 
 #update
-
 echo 'Updating the system'
-
 sudo dnf --refresh upgrade;
 
 #RPMFUSION
-
 echo 'Installing free & nonfree rpmfusion'
-
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 #Codecs
-
 echo 'Installing Necessary Codecs'
-
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing;
 sudo dnf groupupdate multimedia sound-and-video;
 
@@ -41,7 +35,6 @@ sudo dnf install -y rpmfusion-nonfree-release-tainted
 sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware"
 
 #Dragora
-
 echo  'Installing Dragora'
 sudo dnf install -y dnfdragora;
 
@@ -57,11 +50,15 @@ sudo dnf install -y flatseal;
 #Tg
 echo 'Installing Telegram Desktop'
 flatpak install flathub org.telegram.desktop
-#Codium
 
+#Codium
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg;
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo;
 sudo dnf install -y codium;
+
+#AppImageLauncher
+wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
+sudo rpm -i appimagelauncher-2.2.0-travis995.0f91801.x86_64.rpm
 
 #OnlyOffice
 echo 'Removing LibreOffice'
@@ -73,6 +70,8 @@ wget https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/download/v7.
 echo 'Getting Obsidian for ya'
 wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.2.8/Obsidian-1.2.8.AppImage && sudo chmod +x Obsidian-1.2.8.AppImage;
 
+mv -a *.AppImage /home/$USER/Applications
+
 #Alacritty
 echo 'Downloading the Best terminal alacritty' 
 sudo dnf install -y alacritty;
@@ -82,16 +81,9 @@ echo 'Wanna fetch i gotchu Installing fastfetch'
 sudo dnf install -y fastfetch;
 
 #QEMU
-
 echo 'Installing QEMU'
 
 sudo dnf install -y qemu-kvm libvirt virt-install bridge-utils virt-manager libvirt-devel virt-top libguestfs-tools guestfs-tools && sudo systemctl start libvirtd && sudo systemctl enable libvirtd;
-
-
-#ncspot(terminal spotify)
-
-echo 'Installing ncspot'
-flatpak install flathub io.github.hrkfdn.ncspot;
 
 #Spotify
 echo 'Installing Spotiy'
